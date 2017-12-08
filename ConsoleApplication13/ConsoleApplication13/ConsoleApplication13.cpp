28924012820bf568e876d2af5d2ef55c1acd941a
михаил краснослободцев <mihavk2001@gmail.com>
2017-12-08 17:43:47 +0300

    FERST

---
+++ ConsoleApplication13/ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,26 @@
+// ConsoleApplication13.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream> 
+using namespace std;
+void hello();
+int main()
+
+{
+	int n;
+	cin >> n;
+	for ( int i = 0; i < n; i++)
+	{
+		hello();
+	}
+	
+	system("pause");
+
+    return 0;
+}
+void hello ()
+{
+	cout << "aimabear" << endl;
+
+}
